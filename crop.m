54688ed164024c8968b17c79ef9ea2115eec82e9
abhayprakash <abhay.prakash1@gmail.com>
2013-04-15 21:32:20 +0530

    input preprocessing - bw normalized

---
+++ crop.m
@@ -0,0 +1,69 @@
+%% crops the original image
+
+function [crop_img]=crop(original, picX, picY)
+
+%% caluclate size of the input image and make it a square image
+[x, y, z]=size(original);
+if x<y
+    original=imresize(original,[x x]);
+else
+    original=imresize(original,[y y]);
+end
+
+
+%% convert it into black and white
+
+bw1=double(im2bw(original,0.41));
+
+%% add noise on it and then remove it.
+J = imnoise(bw1,'salt & pepper',0.0001);
+J = -1 * (J - 1);
+L = medfilt2(J,[5 5]);
+
+% calculate the size of filtered image
+[x, y, z]=size(L);
+
+% figure('name','black and white');
+% imshow(L);
+
+%% create variables to store the coordinates of the 1st white pixel from each
+% direction
+
+top=0;
+toppos_b=0;
+left=0;
+leftpos_a=0;
+right=0;
+rightpos_a=0;
+bottom=0;
+bottompos_b=0;
+
+%% calculate 1st white pixel from each direction
+for i=1:x-1
+    for j=1:y-1
+        if top==0 && L(i,j)==1 
+            toppos_b=i;
+            top=1;
+        end
+        if left==0 && L(j,i)==1 
+            leftpos_a=i;
+            left=1;
+        end
+        if right==0 && L(j,x-i)==1 
+            rightpos_a=x-i;
+            right=1;
+        end
+        if bottom==0 && L(x-i,j)==1 
+            bottompos_b=x-i;
+            bottom=1;
+        end
+        if top==1 && left ==1 && right==1 && bottom==1
+            break
+        end
+    end
+end
+
+%% crop original image to remove extra background
+crop_img=imcrop(original,[leftpos_a toppos_b rightpos_a-leftpos_a bottompos_b-toppos_b]);
+
+crop_img=imresize(crop_img,[picX picY]);
--- main.m
+++ main.m
@@ -1,18 +1,20 @@
 %% Code for gesture recognition for human sign language - a course project
 %% for Artificial Neural Networks.
+clear all;
 
-clear all
-hand_bw = [];
-for i = 1:5
-    img_name = strcat(num2str(i), '.jpg');
-    hand=imread(img_name); %reading new image
-    %figure
-    %imshow(hand);
-    hand_bw = [hand_bw; im2bw(hand,0.41)];
-end
-imshow(hand_bw);
-img_name = strcat('test.jpg');
-hand=imread(img_name);
+% Contants
+normX = 100;
+normY = 100;
+
+% display original
 figure
-test_bw = im2bw(hand, 0.41);
-imshow(test_bw);
+imshow(imread('1.jpg'));
+y1=crop(imread('1.jpg'), normX, normY);%[1 1 0 0 0; 1 1 0 0 0];
+
+figure
+imshow(y1);
+y2 = im2bw_skin(y1, normX, normY);
+figure
+imshow(y2);
+
+disp('done :)');
